4422a6c4102cdd7dbd463f1c0e7a78a371474a19
EllieByrne2003 <leonbyrne52@gmail.com>
2026-07-27 22:52:34 +0100

    Work on component to get it working.

---
+++ src/component/component.cpp
@@ -0,0 +1,174 @@
+#include "component.hpp"
+
+// Includes from standard
+
+
+// Includes from third party libraries
+
+
+// Includes from personal libraries
+
+
+// Includes from project
+#include "../container/container.hpp"
+#include "../graphics/shaders.hpp"
+
+// Forward declarations
+
+
+// Type aliases
+
+
+
+NLUI::Component::Component() {
+
+}
+
+NLUI::Component::~Component() {
+
+}
+
+void NLUI::Component::setParent(Container *parent) {
+    Container *const oldParent = this->parent;
+    this->parent = nullptr;
+
+    if(oldParent != nullptr) {
+        oldParent->removeComponent(this);
+    }
+
+    this->parent = parent;
+}
+
+void NLUI::Component::removeParent() {
+    Container *const oldParent = this->parent;
+    this->parent = nullptr;    
+    
+    if(oldParent) {
+        oldParent->removeComponent(this);
+    }
+}
+
+void NLUI::Component::draw() const {
+    // TODO implement, accumulate the vertices
+    Graphics::drawSolidQuad(backgroundColour, pos, size);
+}
+
+void NLUI::Component::getMinimumSize(int &minimumWidth, int &minimumHeight) const {
+    minimumWidth  = minimumSize.x;
+    minimumHeight = minimumSize.y;
+}
+
+ivec2 NLUI::Component::getMinimumSize() const {
+    return minimumSize;
+}
+
+int NLUI::Component::getMinimumWidth() const {
+    return minimumSize.x;
+}
+
+int NLUI::Component::getMinimumHeight() const {
+    return minimumSize.y;
+}
+
+void NLUI::Component::setMinimumSize(const int minimumWidth, const int minimumHeight) {
+    minimumSize.x = minimumWidth;
+    minimumSize.y = minimumHeight;
+}
+
+void NLUI::Component::setMinimumSize(const ivec2 &minimumSize) {
+    this->minimumSize = minimumSize;
+}
+
+void NLUI::Component::setMinimumWidth(const int minimumWidth) {
+    minimumSize.x = minimumWidth;
+}
+        
+void NLUI::Component::setMinimumHeight(const int minimumHeight) {
+    minimumSize.y = minimumHeight;
+}
+
+void NLUI::Component::getSize(int &width, int &height) const {
+    width  = size.x;
+    height = size.y;
+}
+
+ivec2 NLUI::Component::getSize() const {
+    return size;
+}
+
+int NLUI::Component::getWidth() const {
+    return size.x;
+}
+
+int NLUI::Component::getHeight() const {
+    return size.y;
+}
+
+void NLUI::Component::setSize(const int width, const int height) {
+    size.x = width;
+    size.y = height;
+}
+
+void NLUI::Component::setSize(const ivec2 &size) {
+    this->size = size;
+}
+        
+void NLUI::Component::setWidth(const int width) {
+    size.x = width;
+}
+
+void NLUI::Component::setHeight(const int height) {
+    size.y = height;
+}
+
+void NLUI::Component::setPos(const int xPos, const int yPos) {
+    pos.x = xPos;
+    pos.y = yPos;
+}
+
+void NLUI::Component::setPos(const ivec2 &pos) {
+    this->pos = pos;
+}
+
+void NLUI::Component::setXPos(const int xPos) {
+    this->pos.x = xPos;
+}
+
+void NLUI::Component::setYPos(const int yPos) {
+    this->pos.y = yPos;
+}
+
+bool NLUI::Component::mouseInside(const double xPos, const double yPos) {
+    // Check minimum
+    if(xPos < pos.x || yPos < pos.y) {
+        return false;
+    }
+
+    // Check maximum
+    if(xPos >= pos.x + size.x || yPos >= pos.y + size.y) {
+        return false;
+    }
+
+    return true;
+}
+
+vec4 NLUI::Component::getBackgroundColour() const {
+    return backgroundColour;
+}
+
+void NLUI::Component::setBackgroundColour(const float r, const float g, const float b, const float a) {
+    backgroundColour.r = r;
+    backgroundColour.g = g;
+    backgroundColour.b = b;
+    backgroundColour.a = a;
+}
+
+void NLUI::Component::setBackgroundColour(const float r, const float g, const float b) {
+    backgroundColour.r = r;
+    backgroundColour.g = g;
+    backgroundColour.b = b;
+}
+
+void NLUI::Component::setBackgroundColour(const vec4 &backgroundColour) {
+    this->backgroundColour = backgroundColour;
+}
--- src/component/component.hpp
+++ src/component/component.hpp
@@ -2,6 +2,8 @@
 
 // Includes from standard
 #include <filesystem>
+#include <memory>
+#include <limits>
 
 // Includes from third party libraries
 #include <glm/glm.hpp>
@@ -18,14 +20,19 @@ using Logger = NLUT::Logger;
 
 
 // Forward declarations
-
+namespace NLUI { class Container; };
 
 // Type aliases
-
+typedef unsigned int GLuint;
 
 namespace NLUI {
     class Component {
     private:
+        Container *parent = nullptr;
+
+        ivec2 minimumSize = ivec2(0, 0);
+
+        vec4 backgroundColour = vec4(0.0f, 0.0f, 0.0f, 0.0f);
 
     protected:
         ivec2 pos;
@@ -33,39 +40,56 @@ namespace NLUI {
 
     public:
         Component();
-        ~Component();
+        virtual ~Component() = 0;
 
-        static Component * readComponent(Logger &logger, const json &jsonComponent, const std::filesystem::path &dirPath);
-        static Component * readComponent(Logger &logger, const std::filesystem::path &path);
+        void setParent(Container *parent);
+        void removeParent();
 
-        virtual void draw() = 0;
+
+        // static Component * readComponent(Logger &logger, const json &jsonComponent, const std::filesystem::path &dirPath);
+        // static Component * readComponent(Logger &logger, const std::filesystem::path &path);
+
+        virtual void draw() const;
 
         // Minimum size
-        virtual void  getMinimumSize(int &minimumWidth, int &minimumHeight) const = 0;
-        virtual ivec2 getMinimumSize() const = 0;
+        virtual void  getMinimumSize(int &minimumWidth, int &minimumHeight) const;
+        virtual ivec2 getMinimumSize() const;
 
-        virtual int getMinimumWidth() const = 0;
-        virtual int getMinimumHeight() const = 0;
+        virtual int getMinimumWidth() const;
+        virtual int getMinimumHeight() const;
 
-        // Maximum size
-        virtual void  getMaximumSize(const int availableWidth, const int availableHeight, int &maximumWidth, int &maximumHeight) const = 0;
-        virtual ivec2 getMaximumSize(const int availableWidth, const int availableHeight) const = 0;
+        void setMinimumSize(const int minimumWidth, const int minimumHeight);
+        void setMinimumSize(const ivec2 &minimumSize);
 
-        virtual void  getMaximumSize(const ivec2 &availableSize, int &maximumWidth, int &maximumHeight) const = 0;
-        virtual ivec2 getMaximumSize(const ivec2 &availableSize) const = 0;
+        void setMinimumWidth(const int minimumWidth);
+        void setMinimumHeight(const int minimumHeight);
 
-        virtual int getMaximumWidth(const int availableWidth) const = 0;
-        virtual int getMaximumHeight(const int availableHeight) const = 0;
+        // TODO may be use this later?
+        // // Maximum size
+        // virtual void  getMaximumSize(const int availableWidth, const int availableHeight, int &maximumWidth, int &maximumHeight) const = 0;
+        // virtual ivec2 getMaximumSize(const int availableWidth, const int availableHeight) const = 0;
+
+        // virtual void  getMaximumSize(const ivec2 &availableSize, int &maximumWidth, int &maximumHeight) const = 0;
+        // virtual ivec2 getMaximumSize(const ivec2 &availableSize) const = 0;
+
+        // virtual int getMaximumWidth(const int availableWidth) const = 0;
+        // virtual int getMaximumHeight(const int availableHeight) const = 0;
 
         // Preferred size
-        virtual void  getPreferredSize(const int availableWidth, const int availableHeight, int &preferredWidth, int &preferredHeight) const = 0;
-        virtual ivec2 getPreferredSize(const int availableWidth, const int availableHeight) const = 0;
+        // virtual void  getPreferredSize(const int availableWidth, const int availableHeight, int &preferredWidth, int &preferredHeight) const = 0;
+        // virtual ivec2 getPreferredSize(const int availableWidth, const int availableHeight) const = 0;
 
-        virtual void  getPreferredSize(const ivec2 &availableSize, int &preferredWidth, int &preferredHeight) const = 0;
-        virtual ivec2 getPreferredSize(const ivec2 &availableSize) const = 0;
+        // virtual void  getPreferredSize(const ivec2 &availableSize, int &preferredWidth, int &preferredHeight) const = 0;
+        // virtual ivec2 getPreferredSize(const ivec2 &availableSize) const = 0;
 
-        virtual int getPreferredWidth(const int availableWidth) const = 0;
-        virtual int getPreferredHeight(const int availableHeight) const = 0;
+        // virtual int getPreferredWidth(const int availableWidth) const = 0;
+        // virtual int getPreferredHeight(const int availableHeight) const = 0;
+
+        virtual void getPreferredSize(int &minimumWidth, int &minimumHeight) const = 0;
+        virtual ivec2 getPreferredSize() const = 0;
+
+        virtual int getPreferredWidth() const = 0;
+        virtual int getPreferredHeight() const = 0;
 
         // Current size
         void  getSize(int &width, int &height) const;
@@ -75,15 +99,29 @@ namespace NLUI {
         int getHeight() const;
 
         // Resizing/positioning
-        virtual void setSize(const int width, const int height);
-        virtual void setSize(const ivec2 &size);
+        void setSize(const int width, const int height);
+        void setSize(const ivec2 &size);
 
-        virtual void setWidth(const int width);
-        virtual void setHeight(const int height);
+        void setWidth(const int width);
+        void setHeight(const int height);
         
         void setPos(const int xPos, const int yPos);
         void setPos(const ivec2 &pos);
+
+        void setXPos(const int xPos);
+        void setYPos(const int yPos);
+
+        virtual void resize() = 0;
         
+        // For handling focus
         virtual bool mouseInside(const double xPos, const double yPos);
+
+        // Coloured backgrounds
+        vec4 getBackgroundColour() const;
+
+        void setBackgroundColour(const float r, const float g, const float b, const float a);
+        void setBackgroundColour(const float r, const float g, const float b);
+
+        void setBackgroundColour(const vec4 &backgroundColour);
     };
 };
