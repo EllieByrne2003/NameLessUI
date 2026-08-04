eb247ef0dc4bbb533f99c7b98875582c6a2619f3
EllieByrne2003 <leonbyrne52@gmail.com>
2026-08-04 22:19:21 +0100

    Oops, last commit stuff.

--- src/graphics/shaders.hpp
+++ src/graphics/shaders.hpp
@@ -16,7 +16,10 @@
 
 
 // Type aliases
+typedef unsigned int GLuint;
+typedef int GLint;
 
+// TODO sizes should all be uvec2 types
 
 namespace NLUI {
     namespace Graphics {
@@ -25,6 +28,9 @@ namespace NLUI {
 
         bool initialiseSolidShader(NLUT::Logger &logger);
         bool solidShaderInitialised();
+
+        bool initialiseTextureShader(NLUT::Logger &logger);
+        bool textureShaderInitialised();
 
         // TODO add other shaders
 
@@ -35,20 +41,29 @@ namespace NLUI {
 
         // Passing uniforms
         void passSolidMVP(const glm::mat4 &mvp); // TODO Don't like this
+        void passTextureMVP(const glm::mat4 &mvp); // TODO or this
 
         // Quadrilateral rendering
         void drawSolidQuad(const glm::vec4 &colour, const glm::vec2 &v1, const glm::vec2 &v2, const glm::vec2 &v3, const glm::vec2 &v4);
         void drawSolidQuad(const glm::vec4 &colour, const glm::vec2 &pos, const glm::vec2 &size);
+
+        // TODO need versions to have separate UVs
+        void drawTextureQuad(const GLuint textureID, const glm::vec2 &v1, const glm::vec2 &v2, const glm::vec2 &v3, const glm::vec2 &v4);
+        void drawTextureQuad(const GLuint textureID, const glm::vec2 &pos, const glm::uvec2 &size);
 
         // void drawSolidQuads();
 
         // Triangle rendering
         void addSolidTri(const glm::vec4 &colour, const glm::vec2 &v1, const glm::vec2 &v2, const glm::vec2 &v3);
 
+        void drawTextureTri(const GLuint textureID, const glm::vec2 &v1, const glm::vec2 &v2, const glm::vec2 &v3);
+
         // void drawSolidTris();
 
 
 
         // TODO add textured stuff
+
+        
     };
 };
